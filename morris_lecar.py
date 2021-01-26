641fbc89e0f1ba1916fd9c7fabc76f53e9d6f89e
mohamedsayed18 <mohamed95.a.s@gmail.com>
2021-01-26 21:28:10 +0300

    make v as function of current

--- morris_lecar.py
+++ morris_lecar.py
@@ -40,28 +40,28 @@ class morris_lecar():
     def t_n(self):
         return 1 / (self.phi * np.cosh((self.v - self.v3) / (2 * self.v4)))
 
-    def dv_dt(self):
+    def dv_dt(self, current):
+        self.I = current
         return (self.I - self.gl * (self.v - self.v_l) - self.gca * self.m_inf() * (self.v - self.v_ca)
             - self.gk * self.n * (self.v - self.v_k)) / self.c
 
     def dn_dt(self):
         return (self.n_ss() - self.n) / self.t_n()
 
-    def get_v(self, t):
+    def get_v(self, current):
         self.volts.append(self.v)
-        self.v = self.v + self.dv_dt() * self.dt
+        self.v = self.v + self.dv_dt(current) * self.dt
         self.n = self.n + self.dn_dt() * self.dt
         
+if __name__ == "__main__":
+    model1 = morris_lecar() # create an instance
 
-model1 = morris_lecar() # create an instance
+    # get the output for 2000 step
+    for i in range(2000):
+        if(i>450 and i<550):
+            model1.get_v(1)
+        else:
+            model1.get_v(0)
 
-# get the output for 2000 step
-for i in range(2000):
-    if(i>450 and i<550):
-        model1.I = 1
-    else:
-        model1.I =0
-    model1.get_v(i)
-
-plt.plot(model1.volts)
-plt.show()
+    plt.plot(model1.volts)
+    plt.show()
