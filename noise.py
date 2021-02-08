748c58eb6547c8ecd98739b8bcd895ed9b7e8710
mohamedsayed18 <mohamed95.a.s@gmail.com>
2021-02-08 10:35:10 +0300

    noise with different amplitude

--- noise.py
+++ noise.py
@@ -9,21 +9,25 @@ import matplotlib.pyplot as plt
 from morris_lecar import morris_lecar
 
 class noisy_model(morris_lecar):
+    D = 0.01
     def dv_dt(self, current):
         self.I = current
         return ((self.I - self.gl * (self.v - self.v_l) - self.gca * self.m_inf() * (self.v - self.v_ca)
-            - self.gk * self.n * (self.v - self.v_k)) / self.c) + np.random.normal(0.001,1)
+            - self.gk * self.n * (self.v - self.v_k)) / self.c) + (noisy_model.D * np.random.normal(0.001,1))
 
 
 if __name__ == "__main__":
     mymodel = noisy_model() # create an instance
 
     # get the output for 2000 step
-    for i in range(2000):
+    for i in range(3000):
         if(i>450 and i<550):
             mymodel.get_v(1)
         else:
-            mymodel.get_v(1)
+            mymodel.get_v(0)
 
-    plt.plot(mymodel.volts)
+    plt.plot(mymodel.volts, label="D = 0.01")
+    plt.xlabel("Time")
+    plt.ylabel("Volt")
+    plt.legend()
     plt.show()
